d9133f39787d85c732136dc6a7fb7abab8e56596
Rafael Guilherme Bertholdo <rafaelgb@administradors-macbook-pro.local>
2016-10-27 09:14:19 -0200

    Initial Commit

---
+++ objectOrientedClass/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  objectOrientedClass
+//
+//  Created by Rafael Guilherme Bertholdo on 10/27/16.
+//  Copyright © 2016 Rafael Guilherme Bertholdo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
