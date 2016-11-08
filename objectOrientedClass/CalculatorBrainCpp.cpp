319640b868c14127cc34e3800f282d0d904d702d
Rafael Guilherme Bertholdo <rafaelgb@Administradors-MacBook-Pro.local>
2016-11-08 11:38:53 -0200

    code cleanup

--- objectOrientedClass/CalculatorBrainCpp.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  CalculatorBrainCpp.cpp
-//  objectOrientedClass
-//
-//  Created by Rafael Guilherme Bertholdo on 11/7/16.
-//  Copyright © 2016 Rafael Guilherme Bertholdo. All rights reserved.
-//
-
-#include "CalculatorBrainCpp.hpp"
